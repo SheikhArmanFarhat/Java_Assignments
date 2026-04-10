79f6fa9680dd251176d52abbab0b1a76ec99a3e4
SheikhArmanFarhat <armanfarhatsheikh.cse24@sbjit.edu.in>
2026-04-11 03:28:59 +0530

    Add Person, Employee, and Manager classes
    Implement class hierarchy with role method for Person, Employee, and Manager.

---
+++ Problem14.java
@@ -0,0 +1,32 @@
+class Person {
+    void role() {
+        System.out.println("I am a Person");
+    }
+}
+
+class Employee extends Person {
+    void role() {
+        System.out.println("I am an Employee");
+    }
+}
+
+class Manager extends Employee {
+    void role() {
+        System.out.println("I am a Manager");
+    }
+}
+
+public class Problem14 {
+    public static void main(String[] args) {
+        Person p;
+
+        p = new Person();
+        p.role();
+
+        p = new Employee();
+        p.role();
+
+        p = new Manager();
+        p.role();
+    }
+}
